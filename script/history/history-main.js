cf1e570c879959c043dad47b24239b2a70d507e0
AnnaCybulska <anna357y@stud.kea.dk>
2022-01-15 12:24:40 +0100

    Cleaning up orders history database

--- script/history/history-main.js
+++ script/history/history-main.js
@@ -7,130 +7,138 @@ let popup = document.querySelector("#info-box");
 let ordersHistory = historyOrders.orders;
 
 const settings = {
-  filter: "all",
-  sortBy: "id",
-  sortDir: "asc",
+    filter: "all",
+    sortBy: "id",
+    sortDir: "asc",
 };
 
 async function start() {
-  registerButtons();
-  loop();
+    registerButtons();
+    loop();
 }
 
 function registerButtons() {
-  document.querySelectorAll("[data-action='sort']").forEach((button) => button.addEventListener("click", selectSort));
+    document
+        .querySelectorAll("[data-action='sort']")
+        .forEach((button) => button.addEventListener("click", selectSort));
 }
 
 function showData(orders) {
-  displayList(orders);
+    displayList(orders);
 }
 
 async function loop() {
-  ordersHistory = historyOrders.orders;
-  showData(ordersHistory);
-  hideLoader();
-  setTimeout(loop, refresh_rate);
+    ordersHistory = historyOrders.orders;
+    showData(ordersHistory);
+    hideLoader();
+    setTimeout(loop, refresh_rate);
 }
 
 ////----- SORTING ------- ////
 
 function selectSort(event) {
-  const sortBy = event.target.dataset.sort;
-  const sortDir = event.target.dataset.sortDirection;
+    const sortBy = event.target.dataset.sort;
+    const sortDir = event.target.dataset.sortDirection;
 
-  //adding styling- indicator of how it is sorted at the moment
-  //find old sort b
-  const old = document.querySelector(`[data-sort="${settings.sortBy}"]`);
-  old.classList.remove("sortby");
-  //indicate active sort
-  event.target.classList.add("sortby");
+    //adding styling- indicator of how it is sorted at the moment
+    //find old sort b
+    const old = document.querySelector(`[data-sort="${settings.sortBy}"]`);
+    old.classList.remove("sortby");
+    //indicate active sort
+    event.target.classList.add("sortby");
 
-  //toggle the direction
-  if (sortDir === "asc") {
-    event.target.dataset.sortDirection = "desc";
-  } else {
-    event.target.dataset.sortDirection = "asc";
-  }
+    //toggle the direction
+    if (sortDir === "asc") {
+        event.target.dataset.sortDirection = "desc";
+    } else {
+        event.target.dataset.sortDirection = "asc";
+    }
 
-  console.log(`user selected ${sortBy} ${sortDir}`);
-  setSort(sortBy, sortDir); // sending  two parameters to the setSort function
+    // console.log(`user selected ${sortBy} ${sortDir}`);
+    setSort(sortBy, sortDir); // sending  two parameters to the setSort function
 }
 
 function setSort(sortBy, sortDir) {
-  settings.sortBy = sortBy; // adding those parameters to the global object
-  settings.sortDir = sortDir;
-  buildList();
+    settings.sortBy = sortBy; // adding those parameters to the global object
+    settings.sortDir = sortDir;
+    buildList();
 }
 
 function sortList(sortedList) {
-  let direction = 1;
+    let direction = 1;
 
-  if (settings.sortDir === "desc") {
-    direction = -1;
-  } else {
-    direction = 1;
-  }
-
-  sortedList = sortedList.sort(sortByProperty);
-
-  function sortByProperty(A, B) {
-    if (A[settings.sortBy] < B[settings.sortBy]) {
-      return -1 * direction;
+    if (settings.sortDir === "desc") {
+        direction = -1;
     } else {
-      return 1 * direction;
+        direction = 1;
     }
-  }
 
-  return sortedList;
+    sortedList = sortedList.sort(sortByProperty);
+
+    function sortByProperty(A, B) {
+        if (A[settings.sortBy] < B[settings.sortBy]) {
+            return -1 * direction;
+        } else {
+            return 1 * direction;
+        }
+    }
+
+    return sortedList;
 }
 
 function buildList() {
-  const sortedList = sortList(ordersHistory);
-  displayList(sortedList);
+    const sortedList = sortList(ordersHistory);
+    displayList(sortedList);
 }
 
 function displayList(orders) {
-  // clear the list
-  document.querySelector("#list tbody").innerHTML = "";
-  // build a new list
-  orders.forEach(displayOrder);
+    // clear the list
+    document.querySelector("#list tbody").innerHTML = "";
+    // build a new list
+    orders.forEach(displayOrder);
 }
 
 function displayOrder(order) {
-  console.log(order);
-  // create clone
-  const clone = document.querySelector("template#order").content.cloneNode(true);
+    // console.log(order);
+    // create clone
+    const clone = document
+        .querySelector("template#order")
+        .content.cloneNode(true);
 
-  // set clone data
-  clone.querySelector("[data-field=id]").textContent = "#" + order.id;
-  clone.querySelector("[data-field=date]").textContent = order.date;
-  clone.querySelector("[data-field=time]").textContent = order.served;
-  clone.querySelector("[data-field=value]").textContent = order.value + " DKK";
-  clone.querySelector("[data-field=bartender]").textContent = order.bartender;
+    // set clone data
+    clone.querySelector("[data-field=id]").textContent = "#" + order.id;
+    clone.querySelector("[data-field=date]").textContent = order.date;
+    clone.querySelector("[data-field=time]").textContent = order.served;
+    clone.querySelector("[data-field=value]").textContent =
+        order.value + " DKK";
+    clone.querySelector("[data-field=bartender]").textContent = order.bartender;
 
-  const details = order.details;
+    const details = order.details;
 
-  clone.querySelector("#row").addEventListener("click", () => showPopUp(details, order.id));
-  //   closePop.addEventListener("click", () => (popup.style.display = "none"));
+    clone
+        .querySelector("#row")
+        .addEventListener("click", () => showPopUp(details, order.id));
+    //   closePop.addEventListener("click", () => (popup.style.display = "none"));
 
-  document.querySelector("#list tbody").appendChild(clone);
+    document.querySelector("#list tbody").appendChild(clone);
 }
 
 ////----- POP UP ------- ////
 
 function showPopUp(order, id) {
-  const lineBreak = order.join("<br> ");
-  popup.classList.remove("hidden");
-  document.querySelector("#list").style.width = "100%";
+    const lineBreak = order.join("<br> ");
+    popup.classList.remove("hidden");
+    document.querySelector("#list").style.width = "100%";
 
-  popup.querySelector(".beer").innerHTML = lineBreak;
-  popup.querySelector("#order-id").innerHTML = "&#9432 ORDER  " + id;
-  popup.querySelector(".total").innerHTML = "Total: " + order.length * 40 + " DKK";
+    popup.querySelector(".beer").innerHTML = lineBreak;
+    popup.querySelector("#order-id").innerHTML = "&#9432 ORDER  " + id;
+    popup.querySelector(".total").innerHTML =
+        "Total: " + order.length * 40 + " DKK";
 
-  document.querySelector("#close-btn").addEventListener("click", hidePopup);
+    document.querySelector("#close-btn").addEventListener("click", hidePopup);
 }
 
 function hidePopup() {
-  popup.classList.add("hidden");
-  document.querySelector("#list").style.width = "131%";
+    popup.classList.add("hidden");
+    document.querySelector("#list").style.width = "131%";
 }
